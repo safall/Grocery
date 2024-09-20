345b86625f32c4f5b601f9c06a301b0087b34b0e
Safal <safal@squareup.com>
2024-09-20 22:16:19 +0200

    Bump version code for release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -18,7 +18,7 @@ android {
         applicationId = "com.whitecatlabs.grocery"
         minSdk = 24
         targetSdk = 34
-        versionCode = 1
+        versionCode = 2
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
