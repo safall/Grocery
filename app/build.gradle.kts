0a866ce8858bfe94cbb8319fe7413dccb5f576ba
Safal <safal.ghimire@tidal.com>
2024-09-22 12:53:50 +0200

    Migration to use hilt

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,9 +1,10 @@
 plugins {
     alias(libs.plugins.android.application)
     alias(libs.plugins.jetbrains.kotlin.android)
-    alias(libs.plugins.compose.compiler)
-    alias(libs.plugins.ksp) apply true
     kotlin("kapt")
+    alias(libs.plugins.compose.compiler)
+    id("com.google.dagger.hilt.android")
+    id("com.google.devtools.ksp")
 }
 
 ksp {
@@ -63,9 +64,7 @@ android {
 }
 
 dependencies {
-    kapt(libs.compilers.dagger)
     ksp(libs.compilers.room)
-    implementation(libs.dagger)
     implementation(libs.androidx.core.ktx)
     implementation(libs.androidx.lifecycle.runtime.ktx)
     implementation(libs.androidx.activity.compose)
@@ -74,7 +73,10 @@ dependencies {
     implementation(libs.androidx.ui.graphics)
     implementation(libs.androidx.ui.tooling.preview)
     implementation(libs.androidx.material3)
-
+    implementation("com.google.dagger:hilt-android:2.51.1")
+    kapt("com.google.dagger:hilt-compiler:2.51.1")
+    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.8.5")
+    implementation("androidx.hilt:hilt-navigation-compose:1.0.0")
     implementation(libs.room.runtime)
     implementation(libs.room.ktx)
     androidTestImplementation(libs.test.androidx.roomTesting)
@@ -85,4 +87,8 @@ dependencies {
     androidTestImplementation(libs.androidx.ui.test.junit4)
     debugImplementation(libs.androidx.ui.tooling)
     debugImplementation(libs.androidx.ui.test.manifest)
+}
+
+kapt {
+    correctErrorTypes = true
 }
