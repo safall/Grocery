50d4afc7d9b9d1cb697ccf44e85a9917894ef398
Safal <safal.ghimire@tidal.com>
2024-09-27 22:31:26 +0200

    Bumped for 1.0.0.5 release

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -19,7 +19,7 @@ android {
         applicationId = "com.whitecatlabs.grocery"
         minSdk = 24
         targetSdk = 34
-        versionCode = 4
+        versionCode = 5
         versionName = "1.0"
 
         testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
